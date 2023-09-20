33dc96b98d4d5e06447a3318f712ca25493ad1d2
Carlos Llano <carlos_eduardo_llano@hotmail.com>
2023-09-20 11:15:05 +0200

    updates

--- memory-stress.sh
+++ memory-stress.sh
@@ -1,1 +1,1 @@
-stress-ng --vm-bytes $(awk '/MemAvailable/{printf "%d\n", $2 * 0.9;}' < /proc/meminfo)k --vm-keep -m 1 --timeout 120s
+stress-ng --vm-bytes $(awk '/MemAvailable/{printf "%d\n", $2 * 0.9;}' < /proc/meminfo)k --vm-keep -m 1 --timeout 240s
